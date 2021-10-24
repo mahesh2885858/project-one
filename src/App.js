1e76191baa31ecf766ad837c8b0053af70688bef
mahesh <mahesh2885858@gmail.com>
2021-10-24 11:51:27 +0530

    second comit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import Home from "./pages/home/Home";
 
 function App() {
-    return (
-        <div>
-            hello
-        </div>
-    )
+  return (
+    <div>
+      <Home />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Topbar from "../../components/topbar/Topbar";
+
+function Home() {
+  return (
+    <>
+      <Topbar />
+    </>
+  );
+}
+
+export default Home;
