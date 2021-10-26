61cd7d81f252a32eb51911a87c7fd79f5f09d5de
mahesh <mahesh2885858@gmail.com>
2021-10-26 11:39:39 +0530

    third

---
+++ src/components/feed/Feed.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Post from "../post/Post";
+import Share from "../share/Share";
+import "./feed.css";
+
+function Feed() {
+  return (
+    <div className="feed">
+      <div className="feedWrapper">
+        <Share />
+        <Post />
+      </div>
+    </div>
+  );
+}
+
+export default Feed;
---
+++ src/components/share/Share.js
@@ -0,0 +1,44 @@
+import { PermMedia, Label, Room, EmojiEmotions } from "@mui/icons-material";
+import React from "react";
+import "./share.css";
+
+function Share() {
+  return (
+    <div className="share">
+      <div className="shareWrapper">
+        <div className="shareTop">
+          <img src="/assets/person/1.jpeg" alt="" className="shareProfileImg" />
+          <input
+            placeholder="what's in your mind mahesh"
+            className="shareInput"
+          />
+        </div>
+        <hr className="shareHr" />
+
+        <div className="shareBottom">
+          <div className="shareOptions">
+            <div className="shareOption">
+              <PermMedia className="shareIcon" />
+              <span className="shareOptionText">photo or video</span>
+            </div>
+            <div className="shareOption">
+              <Label className="shareIcon" />
+              <span className="shareOptionText">Tag</span>
+            </div>
+            <div className="shareOption">
+              <Room className="shareIcon" />
+              <span className="shareOptionText">Location</span>
+            </div>
+            <div className="shareOption">
+              <EmojiEmotions className="shareIcon" />
+              <span className="shareOptionText">Feelings</span>
+            </div>
+          </div>
+          <button className="shareButton">Share</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Share;
